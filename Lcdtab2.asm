a1d149e8ad7554dd7350a70fc33ccc7ea9e18445
魏懿航 <dzzwmqj@outlook.com>
2024-11-19 10:43:52 +0800

    fix: 修复LCD的Com和Seg错误对应的问题

--- Lcdtab2.asm
+++ Lcdtab2.asm
@@ -21,23 +21,23 @@ lcd2_d2	equ	$-lcd_table2
 	db_c_s	c0,s38	; 2G
 
 lcd2_d3	equ	$-lcd_table2
-	db_c_s	c1,s41	; 3A
-	db_c_s	c1,s42	; 3B
-	db_c_s	c0,s42	; 3C
-	db_c_s	c0,s43	; 3D
-	db_c_s	c1,s40	; 3E
-	db_c_s	c0,s40	; 3F
-	db_c_s	c0,s41	; 3G
+	db_c_s	c1,s42	; 3A
+	db_c_s	c1,s43	; 3B
+	db_c_s	c0,s43	; 3C
+	db_c_s	c0,s44	; 3D
+	db_c_s	c0,s41	; 3E
+	db_c_s	c1,s41	; 3F
+	db_c_s	c0,s42	; 3G
 
 lcd2_dot:
 lcd2_PM		equ	$-lcd_table2
 	db_c_s	c1,s32	; PM
 lcd2_bell	equ	$-lcd_table2
-	db_c_s	c1,s43	; bell
+	db_c_s	c1,s44	; bell
 lcd2_DotC	equ	$-lcd_table2
 	db_c_s	c0,s32	; DotC
 lcd2_Zz		equ	$-lcd_table2
-	db_c_s	c0,s44	; Zz
+	db_c_s	c0,s40	; Zz
 
 
 ;==========================================================
@@ -61,15 +61,15 @@ Lcd_bit2:
 	db_c_y	c1,s37	; 2F
 	db_c_y	c0,s38	; 2G
 
-	db_c_y	c1,s41	; 3A
-	db_c_y	c1,s42	; 3B
-	db_c_y	c0,s42	; 3C
-	db_c_y	c0,s43	; 3D
-	db_c_y	c1,s40	; 3E
-	db_c_y	c0,s40	; 3F
-	db_c_y	c0,s41	; 3G
+	db_c_y	c1,s42	; 3A
+	db_c_y	c1,s43	; 3B
+	db_c_y	c0,s43	; 3C
+	db_c_y	c0,s44	; 3D
+	db_c_y	c0,s41	; 3E
+	db_c_y	c1,s41	; 3F
+	db_c_y	c0,s42	; 3G
 
 	db_c_y	c1,s32	; PM
-	db_c_y	c1,s43	; bell
+	db_c_y	c1,s44	; bell
 	db_c_y	c0,s32	; DotC
-	db_c_y	c0,s44	; Zz
+	db_c_y	c0,s40	; Zz
